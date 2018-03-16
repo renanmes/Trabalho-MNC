8fdb48eb9ddd970abea4131bf9783030e8c62d31
Leonardo Oliveira <leosoliveira10@gmail.com>
2018-03-16 09:23:27 -0300

    Criando o arquivo principal

---
+++ Trab1.cpp
@@ -0,0 +1,18 @@
+//  Trabalho 1 - Métodos Numéricos Computacionais
+//  Grupo: 
+//		Gabriel Vieira
+//		Leonardo Silva de Oliveira
+//		Renan Scatolino Mesquita
+
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+	setlocale(LC_ALL,"Portuguese");
+
+}
+
